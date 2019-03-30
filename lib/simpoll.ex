aa62f90c59335aec1a88b08c46d5b3b7c1b53505
Eric Flick <Eric@Erics-MBP.attlocal.net>
2019-03-29 20:14:25 -0500

    First

---
+++ lib/simpoll.ex
@@ -0,0 +1,18 @@
+defmodule Simpoll do
+  @moduledoc """
+  Documentation for Simpoll.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Simpoll.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/simpoll_test.exs
@@ -0,0 +1,8 @@
+defmodule SimpollTest do
+  use ExUnit.Case
+  doctest Simpoll
+
+  test "greets the world" do
+    assert Simpoll.hello() == :world
+  end
+end
